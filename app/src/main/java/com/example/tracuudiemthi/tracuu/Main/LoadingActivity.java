74b4efaea96063b8b49052b973eee224726bbaaa
dangvu2408 <hiend6887@gmail.com>
2024-08-01 15:10:06 +0700

    app version name (final) (01-08-2024)

--- app/src/main/java/com/example/tracuudiemthi/tracuu/Main/LoadingActivity.java
+++ app/src/main/java/com/example/tracuudiemthi/tracuu/Main/LoadingActivity.java
@@ -1,11 +1,14 @@
 package com.example.tracuudiemthi.tracuu.Main;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,11 +18,22 @@ import com.example.tracuudiemthi.tracuu.Utils.CountingData;
 import com.example.tracuudiemthi.tracuu.Utils.DataUtils;
 
 public class LoadingActivity extends AppCompatActivity {
+    private TextView ver;
     public static long start;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loading_layout);
+        ver = findViewById(R.id.appVersionName);
+        try {
+            PackageManager packageManager = getPackageManager();
+            PackageInfo packageInfo = packageManager.getPackageInfo(getPackageName(), 0);
+            String versionName = packageInfo.versionName;
+            String versionText = "Phiên bản " + versionName;
+            ver.setText(versionText);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
         start = System.currentTimeMillis();
         new Create().execute(new Void[0]);
     }
--- app/src/main/java/com/example/tracuudiemthi/tracuu/Main/SplashActivity.java
+++ app/src/main/java/com/example/tracuudiemthi/tracuu/Main/SplashActivity.java
@@ -1,19 +1,33 @@
 package com.example.tracuudiemthi.tracuu.Main;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.tracuudiemthi.R;
 
 public class SplashActivity extends AppCompatActivity {
+    private TextView ver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_layout);
+        ver = findViewById(R.id.appVersionName);
+        try {
+            PackageManager packageManager = getPackageManager();
+            PackageInfo packageInfo = packageManager.getPackageInfo(getPackageName(), 0);
+            String versionName = packageInfo.versionName;
+            String versionText = "Phiên bản " + versionName;
+            ver.setText(versionText);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
